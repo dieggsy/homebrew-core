966ec12971d8fe0614316a89f86900acef9929db
BrewTestBot <1589480+BrewTestBot@users.noreply.github.com>
2025-03-28 10:53:47 +0000

    deno 2.2.6

--- Formula/d/deno.rb
+++ Formula/d/deno.rb
@@ -1,8 +1,8 @@
 class Deno < Formula
   desc "Secure runtime for JavaScript and TypeScript"
   homepage "https://deno.com/"
-  url "https://github.com/denoland/deno/releases/download/v2.2.4/deno_src.tar.gz"
-  sha256 "67808c70241abe878f177767585863d8442214b7a18c0ba7527c4a8f464f8cb3"
+  url "https://github.com/denoland/deno/releases/download/v2.2.6/deno_src.tar.gz"
+  sha256 "e3a0763f10d8f0ec511f2617456c7e0eee130c2b7a6787abbbab3baf29bc98e8"
   license "MIT"
   head "https://github.com/denoland/deno.git", branch: "main"
 
